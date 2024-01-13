d31761071edec142e9eb2f2d3015c45f139fe04a
Seiya Tanaka <seiya@cloudflare.com>
2024-01-13 12:54:32 +0900

    first commit

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,56 @@ export interface Env {
 	//
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	// MY_QUEUE: Queue;
+
+	DB: D1Database;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	
+	async fetch(request: Request, env: Env){
+
+		const corsHeaders = {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+			"Access-Control-Allow-Headers": "*",
+			"Access-Control-Max-Age": "86400",
+		  };
+
+		  if (request.method === "OPTIONS") {
+			return new Response( null, {
+				status: 200,
+				headers: {
+					...corsHeaders,
+				},
+
+		  }) 
+		}   else if (request.method === "POST") {
+			let test = JSON.stringify(await request.json());
+
+			const jsonObject = JSON.parse(test);
+			const fromDate = jsonObject.fromDate;
+            const toDate = jsonObject.toDate;
+			//const { fromDate, toDate } =  test;
+			console.log("ok body")
+			console.log(test);
+			console.log(fromDate);
+			console.log(toDate);
+			const stmt = env.DB.prepare('SELECT COUNT(*) AS NUM, ClientIP FROM http_requests_client WHERE EdgeStartTimestamp BETWEEN ?1 AND ?2 GROUP BY ClientIP ORDER BY NUM DESC;').bind(fromDate, toDate);
+			console.log(stmt);
+			const { results } = await stmt.all();
+			console.log(JSON.stringify(results))
+			return new Response(JSON.stringify(results), {
+			headers: {
+				"content-type": "application/json;charset=UTF-8",
+				...corsHeaders,
+			},
+		});
+		  } else if (request.method === "GET") {
+			return new Response("The request was a GET");
+		  }
+
+
+		
 	},
 };
+
